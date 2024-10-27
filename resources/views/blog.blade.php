24e5fc81ff05e7883a565cae9681bf2153b58e19
rayhanrezki <rayhanalfarezki@gmail.com>
2024-10-27 09:25:28 +0700

    struktur folder v3

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">contact</a>
+    <article>
+      <h1>Evoreyes For New President</h1>
+      Evoreyes akan menguasai dunia dengan banyak cara merangkak dari bawah hidup dari orang orang lemah tak berdaya
+      dan akan mulai merangkak menuju keatas bersama rakyat menengah menjadi mata yang mengawasi seluruh penjara dunia
+
+    </article>
+    
+
+
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Halaman Home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/blog">blog</a>
+  <a href="/contact">contact</a>
+  <h1>Contact</h1>
+  <ul>
+    <li>
+      <a href="https://www.instagram.com/rayhan_rezki
+">rayhan_rezki</a>
+    </li>
+  </ul>
+  
+  
+</body>
+</html>
