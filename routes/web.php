af522fe6a8bd3e28ee87ec95c426fe5a3bc48449
rayhanrezki <rayhanalfarezki@gmail.com>
2024-11-03 06:28:15 +0700

    video 7

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -12,55 +13,13 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'judul artikel 1',
-            'author' => 'Ray',
-            'body' => ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe esse ipsum praesentium,
-               molestias dolor ducimus, dolorum neque, aliquid iusto animi quo. Harum, similique consequuntur facere
-               minus provident quasi dicta dolorrsas',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'judul artikel 2',
-            'author' => 'Ray',
-            'body' => ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe esse ipsum praesentium,
-               molestias dolor ducimus, dolorum neque, aliquid iusto animi quo. Harum, similique consequuntur facere
-               minus provident quasi dicta dolorrsas',
-        ],
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all() ]);
 });
 
 Route::get('/post/{slug}', function ($slug) {
 
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'judul artikel 1',
-            'author' => 'Ray',
-            'body' => ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe esse ipsum praesentium,
-               molestias dolor ducimus, dolorum neque, aliquid iusto animi quo. Harum, similique consequuntur facere
-               minus provident quasi dicta dolorrsas',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'judul artikel 2',
-            'author' => 'Ray',
-            'body' => ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe esse ipsum praesentium,
-               molestias dolor ducimus, dolorum neque, aliquid iusto animi quo. Harum, similique consequuntur facere
-               minus provident quasi dicta dolorrsas',
-        ],
-    ];
-
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
-    return view('post', ['title' => 'Single Post', 'post' => $post]);
+    $Post = Post::find($slug);
+    return view('post', ['title' => 'Single Post', 'post' => $Post]);
 
 });
 
