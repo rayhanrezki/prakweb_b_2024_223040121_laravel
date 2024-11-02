58570beefef52788a355ed130e2d71d7e6e1bde0
rayhanrezki <rayhanalfarezki@gmail.com>
2024-11-03 02:32:30 +0700

    video 5

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Halaman Home</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-  <a href="/blog">blog</a>
-  <a href="/contact">contact</a>
-  <h1>Contact</h1>
-  <ul>
-    <li>
-      <a href="https://www.instagram.com/rayhan_rezki
-">rayhan_rezki</a>
-    </li>
-  </ul>
-  
-  
-</body>
-</html>
+
+<x-layout>
+  <x-slot:title> {{  $title  }}</x-slot:title>
+  <h3 class ='text-xl:'>Halaman contact </h3>
+
+</x-layout>
