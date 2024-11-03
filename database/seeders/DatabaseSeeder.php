3ff04beb4e0e578254a3c24c367b78b2dc9f48d9
rayhanrezki <rayhanalfarezki@gmail.com>
2024-11-03 16:46:08 +0700

    video 13

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+//
+        // Category::create([
+        //     'name' => 'web-design',
+        //     'slug' => 'web-design',
+        // ]);
+//
+        // Post::create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Of course, manually specifying the attributes for each model seed is cumbersome.
+        //     Instead, you can use model factories to conveniently generate large amounts of database records.
+        //     First, review the model factory documentation to learn how to define your factories.',
+        // ]);
+
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all(),
+        ])->create();
+
     }
 }
